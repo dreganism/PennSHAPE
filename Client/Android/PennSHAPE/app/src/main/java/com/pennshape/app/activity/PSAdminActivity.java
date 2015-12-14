0d5bc3cebcaed43fd22f62062648364a41b29615
Sun, Han (Jeremiah) <hasun@microstrategy.com>
2015-12-14 00:04:53 -0500

    send admin message

--- Client/Android/PennSHAPE/app/src/main/java/com/pennshape/app/activity/PSAdminActivity.java
+++ Client/Android/PennSHAPE/app/src/main/java/com/pennshape/app/activity/PSAdminActivity.java
@@ -1,13 +1,27 @@
 package com.pennshape.app.activity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
 import com.pennshape.app.R;
+import com.pennshape.app.model.PSDataStore;
+import com.pennshape.app.request.PSHttpTaskRequest;
+import com.pennshape.app.request.PSMessagePullTaskRequest;
+import com.pennshape.app.request.PSMessageSendTaskRequest;
 
-public class PSAdminActivity extends Activity {
+import org.json.JSONArray;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class PSAdminActivity extends Activity implements PSHttpTaskRequest.PSHttpTaskRequestHandler{
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US);
+    private ProgressDialog progress;
     private EditText editText;
 
     @Override
@@ -29,6 +43,38 @@ public class PSAdminActivity extends Activity {
     }
 
     private void postMessage(String message){
+        PSMessageSendTaskRequest request = new PSMessageSendTaskRequest();
+        request.setUserID("0");
+        request.setGroupid("0");
+        request.setMsg(message);
+        request.setTime(dateFormat.format(Calendar.getInstance().getTime()));
+        request.handler = this;
+        request.run();
+        if(progress!= null) progress.dismiss();
+        progress = ProgressDialog.show(this, "Loading...", "Please wait", true);
+    }
 
+    @Override
+    public void onSuccess(PSHttpTaskRequest request, Object result) {
+        if(request instanceof PSMessageSendTaskRequest){
+            new AlertDialog.Builder(this)
+                    .setTitle("Message Sent")
+                    .setPositiveButton(android.R.string.ok, null)
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+            editText.setText("");
+        }
+        if(progress!= null) progress.dismiss();
+    }
+
+    @Override
+    public void onFailure(PSHttpTaskRequest request, String error) {
+        if(progress!= null) progress.dismiss();
+        new AlertDialog.Builder(this)
+                .setTitle("Error")
+                .setMessage(error)
+                .setPositiveButton(android.R.string.ok, null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
     }
 }
